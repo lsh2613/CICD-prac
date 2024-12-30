23af9898bd0b4778be2e99460e561f45865b8445
LSH <lsh2613@naver.com>
2024-12-30 16:34:33 +0900

    [test] ci test

---
+++ src/main/java/com/cicdprac/RedisUtil.java
@@ -0,0 +1,15 @@
+package com.cicdprac;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisUtil {
+    @Autowired
+    private final RedisTemplate<String, Long> refreshTokenTemplate;
+
+    public RedisUtil(RedisTemplate<String, Long> refreshTokenTemplate) {
+        this.refreshTokenTemplate = refreshTokenTemplate;
+    }
+}
