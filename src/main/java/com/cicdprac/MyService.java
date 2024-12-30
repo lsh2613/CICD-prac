237228ff701987509503620b7d0878f172016481
LSH <lsh2613@naver.com>
2024-12-30 16:38:11 +0900

    [test] ci test

--- src/main/java/com/cicdprac/MyService.java
+++ src/main/java/com/cicdprac/MyService.java
@@ -1,12 +1,23 @@
 package com.cicdprac;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyService {
 
+    @Autowired
+    private final RedisUtil redisUtil;
+
+    public MyService(RedisUtil redisUtil) {
+        this.redisUtil = redisUtil;
+    }
+
     public String api() {
         return "API is success!";
     }
 
+    public String redis() {
+        return redisUtil.toString();
+    }
 }
